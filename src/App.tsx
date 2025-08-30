2591df37f8f7eb1690a8bbd7768b66029520ca74
zack-young-ideas <zackyoung8709@gmail.com>
2025-08-30 18:23:18 -0400

    Retrieve CSRF token upon page load
    Define the App component to retrieve a CSRF token from the server
    when the component is first mounted. This token is inserted into
    a hidden form field in the contact form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
+function App({ url }: { url: String }) {
+
+  const [csrfToken, setCsrfToken] = useState('');
+
+  useEffect(() => {
+    /*
+    Retrieves a CSRF token upon loading the contact form.
+    */
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Unable to retrieve CSRF token');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setCsrfToken(data.token);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
+      <form>
+        <input
+          data-testid="csrf_token"
+          name="csrf_token"
+          type="hidden"
+          value={csrfToken}
+        />
+      </form>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App url={`${import.meta.env.VITE_SERVER_HOSTNAME}/contact`}/>
   </StrictMode>,
 )
