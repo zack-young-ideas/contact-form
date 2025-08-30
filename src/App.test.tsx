75dada4bb15917a6a18b5f4fbe5dc3bc46a385cd
Zack Young <zyoung612@gmail.com>
2025-08-30 18:23:18 -0400

    Retrieve CSRF token upon page load
    Define the App component to retrieve a CSRF token from the server
    when the component is first mounted. This token is inserted into
    a hidden form field in the contact form.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
 import App from './App';
 
+const server = setupServer(
+  http.get('/contact', () => {
+    return HttpResponse.json({ token: 'randomToken' });
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
 describe('App', () => {
-  it('renders the correct text', () => {
-    render(<App />);
-    expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+  it('retrieves CSRF token upon loading', async () => {
+    render(<App url="/contact" />);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('csrf_token')).toHaveValue('randomToken');
+    });
   });
 });
