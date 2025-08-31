118e1a94f5d4f8639edf47dee220fea2ae08b091
Zack Young <zyoung612@gmail.com>
2025-08-30 20:15:22 -0400

    Define logic to handle form submission
    The App component now sends a POST request to the server any
    time a user submits the contact form.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
 import App from './App';
@@ -6,6 +6,10 @@ import App from './App';
 const server = setupServer(
   http.get('/contact', () => {
     return HttpResponse.json({ token: 'randomToken' });
+  }),
+
+  http.post('/contact', () => {
+    return HttpResponse.json({ message: 'Success' });
   }),
 );
 
@@ -22,7 +26,7 @@ describe('App', () => {
     });
   });
 
-  it('displays error message', async () => {
+  it('displays error message if CSRF token is unavailable', async () => {
     server.use(
       http.get('/contact', () => {
         return new HttpResponse.json(null, { status: 500 });
@@ -30,6 +34,62 @@ describe('App', () => {
     );
 
     render(<App url="/contact" />);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('error-message'))
+        .toHaveTextContent('Unable to load contact form');
+    });
+  });
+
+  it('submits form data', async () => {
+    render(<App url="/contact" />);
+
+    const firstNameField = await screen.getByTestId('first-name');
+    const lastNameField = await screen.getByTestId('last-name');
+    const emailField = await screen.getByTestId('email-address');
+    const phoneField = await screen.getByTestId('phone-number');
+    const messageField = await screen.getByTestId('message');
+    const submitButton = await screen.getByRole('button', { name: 'Submit' });
+
+    fireEvent.change(firstNameField, { target: { value: 'John' } });
+    fireEvent.change(lastNameField, { target: { value: 'Smith' } });
+    fireEvent.change(emailField, { target: { value: 'jsmith@example.com' } });
+    fireEvent.change(phoneField, { target: { value: '1234567890' } });
+    fireEvent.change(
+      messageField, { target: { value: 'I would love to hear from you.' } }
+    );
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('success-message'))
+        .toHaveTextContent('Form submitted successfully');
+    });
+  });
+
+  it('displays error message if POST request fails', async () => {
+    server.use(
+      http.post('/contact', () => {
+        return new HttpResponse.json(null, { status: 500 });
+      }),
+    );
+
+    render(<App url="/contact" />);
+
+    const firstNameField = await screen.getByTestId('first-name');
+    const lastNameField = await screen.getByTestId('last-name');
+    const emailField = await screen.getByTestId('email-address');
+    const phoneField = await screen.getByTestId('phone-number');
+    const messageField = await screen.getByTestId('message');
+    const submitButton = await screen.getByRole('button', { name: 'Submit' });
+
+    fireEvent.change(firstNameField, { target: { value: 'John' } });
+    fireEvent.change(lastNameField, { target: { value: 'Smith' } });
+    fireEvent.change(emailField, { target: { value: 'jsmith@example.com' } });
+    fireEvent.change(phoneField, { target: { value: '1234567890' } });
+    fireEvent.change(
+      messageField, { target: { value: 'I would love to hear from you.' } }
+    );
+    fireEvent.click(submitButton);
 
     await waitFor(() => {
       expect(screen.getByTestId('error-message'))
