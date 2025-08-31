28e8967c191388fae22dc742bcf3790ef0fe6bd5
zack-young-ideas <zackyoung8709@gmail.com>
2025-08-30 20:15:22 -0400

    Define logic to handle form submission
    The App component now sends a POST request to the server any
    time a user submits the contact form.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import './App.css'
 function App({ url }: { url: String }) {
 
   const [csrfToken, setCsrfToken] = useState('');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
   const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     /*
@@ -25,13 +33,83 @@ function App({ url }: { url: String }) {
       });
   }, []);
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  const submitForm = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Unable to submit form');
+      }
+      setSuccess(true);
+    } catch (error) {
+      setError(true);
+    }
+  }
+
   const errorMessage = (
     <p data-testid="error-message">Unable to load contact form</p>
+  );
+  const successMessage = (
+    <p data-testid="success-message">Form submitted successfully</p>
   );
 
   return (
     <>
-      <form>
+      <form onSubmit={submitForm}>
+        <input
+          data-testid="first-name"
+          name="first_name"
+          onChange={handleChange}
+          placeholder="First Name"
+          type="text"
+          value={formData.firstName}
+        />
+        <input
+          data-testid="last-name"
+          name="last_name"
+          onChange={handleChange}
+          placeholder="Last Name"
+          type="text"
+          value={formData.lastName}
+        />
+        <input
+          data-testid="email-address"
+          name="email_address"
+          onChange={handleChange}
+          placeholder="Email Address"
+          type="email"
+          value={formData.email}
+        />
+        <input
+          data-testid="phone-number"
+          name="phone_number"
+          onChange={handleChange}
+          placeholder="Phone Number"
+          type="text"
+          value={formData.phone}
+        />
+        <textarea
+          data-testid="message"
+          name="message"
+          onChange={handleChange}
+          placeholder="Message"
+          value={formData.message}
+        ></textarea>
         <input
           data-testid="csrf-token"
           name="csrf_token"
@@ -39,7 +117,8 @@ function App({ url }: { url: String }) {
           value={csrfToken}
         />
         {error && errorMessage}
-        <button>Submit</button>
+        {success && successMessage}
+        <button type="submit">Submit</button>
       </form>
     </>
   )
