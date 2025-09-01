7bbca6c3bf330d69af12636245784fa2d47ee236
Zack Young <zyoung612@gmail.com>
2025-09-01 01:00:46 -0400

    Add spinning animation and improve tests
    A new spinning animation was added. It is displayed while the
    app attempts to retrieve a CSRF token from the server.
    
    Furthermore, several tests were rewritten to make them more
    robust. They now better ensure that the app works the way it
    should.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 function App({ url }: { url: String }) {
 
   const [csrfToken, setCsrfToken] = useState('');
+  const [spinner, setSpinner] = useState(true);
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
@@ -11,7 +12,7 @@ function App({ url }: { url: String }) {
     phone: '',
     message: '',
   });
-  const [error, setError] = useState(false);
+  const [error, setError] = useState('');
   const [success, setSuccess] = useState(false);
 
   useEffect(() => {
@@ -27,9 +28,11 @@ function App({ url }: { url: String }) {
       })
       .then(data => {
         setCsrfToken(data.token);
+        setSpinner(false);
       })
       .catch(error => {
-        setError(true);
+        setError('Unable to load contact form');
+        setSpinner(false);
       });
   }, []);
 
@@ -44,6 +47,7 @@ function App({ url }: { url: String }) {
   const submitForm = async (event) => {
     event.preventDefault();
     try {
+      formData.csrfToken = csrfToken;
       const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -52,86 +56,96 @@ function App({ url }: { url: String }) {
         body: JSON.stringify(formData),
       });
 
+      const data = await response.json();
       if (!response.ok) {
-        throw new Error('Unable to submit form');
+        throw new Error(data.message);
       }
       setSuccess(true);
     } catch (error) {
-      setError(true);
+      setError(error.message);
     }
   }
 
-  const errorMessage = (
+  const errorContent = (
     <p
       data-testid="error-message"
       id="contact-form-error-message"
     >
-      Unable to load contact form
-    </p>
-  );
-  const successMessage = (
-    <p
-      data-testid="success-message"
-      id="contact-form-success-message"
-    >
-      Form submitted successfully
+      {error}
     </p>
   );
 
-  return (
-    <>
-      <form onSubmit={submitForm}>
-        <input
-          data-testid="first-name"
-          name="first_name"
-          onChange={handleChange}
-          placeholder="First Name"
-          type="text"
-          value={formData.firstName}
-        />
-        <input
-          data-testid="last-name"
-          name="last_name"
-          onChange={handleChange}
-          placeholder="Last Name"
-          type="text"
-          value={formData.lastName}
-        />
-        <input
-          data-testid="email-address"
-          name="email_address"
-          onChange={handleChange}
-          placeholder="Email Address"
-          type="email"
-          value={formData.email}
-        />
-        <input
-          data-testid="phone-number"
-          name="phone_number"
-          onChange={handleChange}
-          placeholder="Phone Number"
-          type="text"
-          value={formData.phone}
-        />
-        <textarea
-          data-testid="message"
-          name="message"
-          onChange={handleChange}
-          placeholder="Message"
-          value={formData.message}
-        ></textarea>
-        <input
-          data-testid="csrf-token"
-          name="csrf_token"
-          type="hidden"
-          value={csrfToken}
-        />
-        {error && errorMessage}
-        {success && successMessage}
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  )
+  let content;
+  if (success) {
+    content = (
+      <div>
+        <p
+          data-testid="success-message"
+          id="contact-form-success-message"
+        >
+          Form submitted successfully
+        </p>
+      </div>
+    );
+  } else if (spinner) {
+    content = <div id="contact-form-spinner"></div>
+  } else {
+    content = (
+      <>
+        <form onSubmit={submitForm}>
+          <input
+            data-testid="first-name"
+            name="firstName"
+            onChange={handleChange}
+            placeholder="First Name"
+            type="text"
+            value={formData.firstName}
+          />
+          <input
+            data-testid="last-name"
+            name="lastName"
+            onChange={handleChange}
+            placeholder="Last Name"
+            type="text"
+            value={formData.lastName}
+          />
+          <input
+            data-testid="email-address"
+            name="email"
+            onChange={handleChange}
+            placeholder="Email Address"
+            type="email"
+            value={formData.email}
+          />
+          <input
+            data-testid="phone-number"
+            name="phone"
+            onChange={handleChange}
+            placeholder="Phone Number"
+            type="text"
+            value={formData.phone}
+          />
+          <textarea
+            data-testid="message"
+            name="message"
+            onChange={handleChange}
+            placeholder="Message"
+            value={formData.message}
+          ></textarea>
+          <input
+            data-testid="csrf-token"
+            name="csrfToken"
+            type="hidden"
+            value={csrfToken}
+          />
+          {error && errorContent}
+          <button type="submit">Submit</button>
+        </form>
+      </>
+    )
+  }
+
+  return content;
 }
 
 export default App
